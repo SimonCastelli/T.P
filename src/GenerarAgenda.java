6ba06ddff2b844dd97e57414323904e6ddd2f467
gonza <gonzaoshiro@gmail.com>
2025-09-09 10:49:52 -0300

    Prueba bis

--- src/GenerarAgenda.java
+++ src/GenerarAgenda.java
@@ -60,6 +60,7 @@ public class GenerarAgenda {
             }
             raf.writeUTF(" hola");
             raf.writeUTF("Prueba");
+            raf.writeUTF("github");
         raf.close();
     }
 }
